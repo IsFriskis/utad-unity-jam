d50ed99b88893e34064992596fa02e08ae46c881
maltrak007 <gonza7108@gmail.com>
2024-01-17 18:23:22 +0100

    Version Final Gonzalo

--- UnityJam/Assets/Scripts/Enemies/FlyingEye/FlyingEyeScript.cs
+++ UnityJam/Assets/Scripts/Enemies/FlyingEye/FlyingEyeScript.cs
@@ -30,11 +30,11 @@ public class FlyingEyeScript : MonoBehaviour
         //Flip al sprite en función de la velocidad
         if (aiPath.desiredVelocity.x >= 0.01f)
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
         }
         else if (aiPath.desiredVelocity.x <= -0.01f)
         {
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
         }
     }
 
